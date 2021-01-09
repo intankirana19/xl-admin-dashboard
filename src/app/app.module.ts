fad196e6da1baac4f9a09c39393dfeb5034abc3d
intankirana19 <intankirana19@gmail.com>
2021-01-09 13:36:18 +0700

    add authguard,auth routing, not-found comp, layout comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    NotFoundComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
